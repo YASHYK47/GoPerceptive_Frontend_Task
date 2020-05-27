2379264d9927c06340c2f51757c1ab1de1cd72ad
YASHYK47 <yashky47@gmail.com>
2020-05-28 03:19:27 +0530

    Event View Completed

--- src/components/event_view.js
+++ src/components/event_view.js
@@ -6,8 +6,47 @@ class ViewEvent extends Component {
     const { event } = this.props.location.state;
     console.log(event);
     return (
-      <div>
-        <h1>Event View</h1>
+      <div
+        className="ui clearing segment"
+        style={{
+          backgroundColor: "rgb(208, 199, 254)",
+          border: "4px solid rgb(101, 107, 128)",
+          borderRadius: "10px",
+        }}
+      >
+        <h1
+          className="ui center aligned header"
+          style={{ textDecoration: "underline" }}
+        >
+          Event Details
+        </h1>
+        <h2>
+          <span style={{ textDecoration: "underline" }}>Title</span> :{" "}
+          {event.title}
+        </h2>
+        <div className="inline ui grid">
+          <h2 className="eight wide column">
+            <span style={{ textDecoration: "underline" }}>Category</span> :{" "}
+            {event.category}
+          </h2>
+          <h2 className="eight wide column" style={{ margin: "0px" }}>
+            <span style={{ textDecoration: "underline" }}>Date</span> :{" "}
+            {event.date}
+          </h2>
+        </div>
+        <h2 style={{ marginTop: "0px" }}>
+          <span style={{ textDecoration: "underline" }}>Description</span> :{" "}
+        </h2>
+        <p
+          style={{
+            fontSize: "20px",
+            border: "2px solid rgb(101, 107, 128)",
+            borderRadius: "5px",
+            padding: "5px",
+          }}
+        >
+          {event.description}
+        </p>
       </div>
     );
   }
--- src/reducers/events_reducers.js
+++ src/reducers/events_reducers.js
@@ -1,6 +1,17 @@
 import { CREATE_EVENT } from "../actions";
 
-export default function (state = [{ title: "title", date: "date" }], action) {
+export default function (
+  state = [
+    {
+      title: "Lorem",
+      date: "2020-05-17",
+      category: "Ipsum",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in arcu sit amet tortor faucibus dictum in rutrum mi. Ut mollis commodo faucibus. Suspendisse a rhoncus neque, nec ultrices mi. Nunc vestibulum quam eros, sed blandit orci sagittis id. Curabitur sed mi lectus. Integer in congue urna. Fusce eu lacinia erat. In vitae orci nisi. Ut tempor dapibus quam, ac aliquet nisl pharetra aliquet. Quisque lacinia, libero bibendum blandit vulputate, arcu libero sagittis mi, nec consectetur enim lectus quis magna. Etiam in tellus maximus, mollis odio nec, porttitor nisi. Integer dapibus vehicula tristique. Nunc venenatis fringilla lacus, quis laoreet nisi placerat a.",
+    },
+  ],
+  action
+) {
   switch (action.type) {
     case CREATE_EVENT:
       return [...state, action.payload];
