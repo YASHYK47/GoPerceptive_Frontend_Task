1de3d451aeb81238ddce23d31b703bd8bcaa4d54
YASHYK47 <yashky47@gmail.com>
2020-05-28 03:35:39 +0530

    added all-page header

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,20 @@ class App extends Component {
   render() {
     return (
       <div className="ui container" style={{ marginTop: "25px" }}>
+        <h1
+          className="ui header center aligned"
+          style={{
+            backgroundColor: "rgb(208, 199, 254)",
+            border: "4px solid rgb(0,0,0)",
+            borderRadius: "10px",
+            padding: "10px",
+            width: "25%",
+            marginLeft: "35%",
+            fontWeight: "1800",
+          }}
+        >
+          Event Manager
+        </h1>
         {this.props.children}
       </div>
     );
